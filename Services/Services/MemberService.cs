efdb76f93e3d787102ec409339989d3b4d081f44
unknown <kontit543@gmail.com>
2024-08-21 15:35:40 +0700

    first commit

---
+++ Services/Services/MemberService.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class MemberService
+    {
+        public readonly static MemberService Instance = new MemberService();
+        private readonly UserRepository userRepository = UserRepository.Instance;
+
+        static MemberService() { }
+
+        public void AddNewMember(string username, string password)
+        {
+            var newMember = new Member()
+            {
+                Username = username,
+                Password = password
+            };
+
+            userRepository.Save(newMember);
+        }
+
+        public void DisableMember(string username)
+        {
+            var userToDisable = userRepository.GetByUsername(username);
+
+            if (userToDisable is null || userToDisable is not Member)
+                return;
+
+            var user = (Member)userToDisable;
+            user.IsDisabled = true;
+            userRepository.Save(user);
+        }
+        public void UpdatePassword(string username, string newMemberPassword)
+        {
+            var userToUpdate = userRepository.GetByUsername(username);
+
+            if (userToUpdate is null || userToUpdate is not Member)
+                return;
+
+            var user = (Member)userToUpdate;
+            user.Password = newMemberPassword;
+            userRepository.Update(user);
+        }
+        public void DeleteMember(string username)
+        {
+            var userToDelete = userRepository.GetByUsername(username);
+
+            if (userToDelete is null || userToDelete is not Member)
+                return;
+
+            var user = (Member)userToDelete;
+            userRepository.Delete(user);
+        }
+    }
+}
---
+++ UserManagement/UserInterfaces/AdminPage.cs
@@ -0,0 +1,120 @@
+﻿using Services.Dtos;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace UserManagement.UserInterfaces
+{
+    public static class AdminPage
+    {
+        private static LoginService loginService = LoginService.Instance;
+        private static MemberService memberService = MemberService.Instance;
+        private static LoginInfo loginInfo = loginService.GetLoginInfo();
+
+        private static List<string> options = new List<string>()
+        {
+            "Add member account",
+            "Disable member account",
+            "Update member password", //Update member password interface
+            "Delete member account", //Delete member account interface
+            "Logout"
+        };
+
+        public static void Start()
+        {
+            while (loginInfo.IsLoggedIn)
+            {
+                var choice = Helpers.GetChoice(options);
+
+                if (choice == options.Count)
+                {
+                    loginService.Logout();
+                    Console.WriteLine("You've logged out.");
+                    continue;
+                }
+
+                if (choice == 1)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter member username:");
+                        var username = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Enter member password:");
+                        var password = Console.ReadLine();
+
+                        if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                            continue;
+
+                        memberService.AddNewMember(username, password);
+                        Console.WriteLine("\nSuccess to add member");
+                    }
+                    catch(Exception ex)
+                    {
+                        continue;
+                    }
+                }
+
+                if (choice == 2)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter member username:");
+                        var username = Console.ReadLine();
+                        Console.WriteLine();
+                        
+                        memberService.DisableMember(username);
+                        Console.WriteLine("\nYour password was disabled");
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                if (choice == 3)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter member username:");
+                        var username = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Enter new member password:");
+                        var password = Console.ReadLine();
+
+                        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                            continue;
+
+                        memberService.UpdatePassword(username, password);
+                        Console.WriteLine("\nYour password was updated");
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+                if (choice == 4)
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter member username:");
+                        var username = Console.ReadLine();
+                        Console.WriteLine();
+
+                        memberService.DeleteMember(username);
+                        Console.WriteLine($"Member {username} was deleted");
+                    }
+                    catch (Exception ex)
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
